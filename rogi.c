e3cdeee8f2ea197ad022d22b8375c88802412a37
kowal2000 <59841365+kowal2000@users.noreply.github.com>
2020-04-08 19:50:14 +0200

    zmiany w rogi.c

--- rogi.c
+++ rogi.c
@@ -8,7 +8,7 @@ dane = fopen(nazwa, "r");
         printf("error in opening file dane");
 
     rogi_t tab_rogow[200];
-    int c;
+    int c,k,l;
     int s=0;
     int tab_wejsciowa[40][80]={{0}};
     for(int i=0; i<40; i++){      // zamiana danych wejsciowych na tablice, gdzie *=-9, #=0
@@ -23,60 +23,110 @@ dane = fopen(nazwa, "r");
         for(int j=1; j<79; j++){
             if(tab_wejsciowa[i][j]==0){
                 if(tab_wejsciowa[i-1][j]==-9 && tab_wejsciowa[i][j-1]==-9){
-                    tab_rogow[s].x=100*j;
+		    c=1;
+                    for(k=1; k<=4; k++)
+			if(tab_wejsciowa[i+k][j]==-9 || tab_wejsciowa[i][j+k]==-9)
+				c=0;
+		    if(c==1){
+		    tab_rogow[s].x=100*j;
                     tab_rogow[s].y=100*i;
                     tab_rogow[s].kat=90;
                     tab_rogow[s].kat_obrotu=270;
                     s++;
+		    }
                 }
 		if(tab_wejsciowa[i-1][j]==-9 && tab_wejsciowa[i][j+1]==-9){
+		    c=1;
+		    for(k=1; k<=4; k++)
+			for(l=1; l<=9; l++)
+                            if(tab_wejsciowa[i+k][j]==-9 || tab_wejsciowa[i][j-l]==-9)
+                                c=0;
+		    if(c==1){
                     tab_rogow[s].x=100*(j+1);
                     tab_rogow[s].y=100*i;
                     tab_rogow[s].kat=90;
                     tab_rogow[s].kat_obrotu=180;
                     s++;
+		    }
                 }
                 if(tab_wejsciowa[i+1][j]==-9 && tab_wejsciowa[i][j-1]==-9){
+		    c=1;
+		    for(k=1; k<=4; k++)
+                        for(l=1; l<=9; l++)
+                            if(tab_wejsciowa[i-l][j]==-9 || tab_wejsciowa[i][j+k]==-9)
+                                c=0;
+                    if(c==1){
                     tab_rogow[s].x=100*j;
                     tab_rogow[s].y=100*(i+1);
                     tab_rogow[s].kat=90;
                     tab_rogow[s].kat_obrotu=0;
                     s++;
+		    }
                 }
                 if(tab_wejsciowa[i+1][j]==-9 && tab_wejsciowa[i][j+1]==-9){
+		    c=1;
+                    for(k=1; k<=9; k++)
+			    if(tab_wejsciowa[i-k][j]==-9 || tab_wejsciowa[i][j-k]==-9)
+			    c=0;
+		    if(c==1){
                     tab_rogow[s].x=100*(j+1);
                     tab_rogow[s].y=100*(i+1);
                     tab_rogow[s].kat=90;
                     tab_rogow[s].kat_obrotu=90;
                     s++;
+		    }
                 }
                 if(tab_wejsciowa[i+1][j+1]==-9 && tab_wejsciowa[i+1][j]==0 && tab_wejsciowa[i][j+1]==0){
+		    c=1;
+                    for(k=1; k<=7; k++)
+                        if(tab_wejsciowa[i+k][j]==-9 || tab_wejsciowa[i][j+k]==-9)
+                                c=0;
+                    if(c==1){
                     tab_rogow[s].x=100*(j+1);
                     tab_rogow[s].y=100*(i+1);
                     tab_rogow[s].kat=270;
                     tab_rogow[s].kat_obrotu=270;
                     s++;
+		    }
 		}
 	 	if(tab_wejsciowa[i+1][j-1]==-9 && tab_wejsciowa[i+1][j]==0 && tab_wejsciowa[i][j-1]==0){
+		    c=1;
+                    for(k=1; k<=7; k++)
+                        if(tab_wejsciowa[i+k][j]==-9 || tab_wejsciowa[i][j-k]==-9)
+                                c=0;
+                    if(c==1){
                     tab_rogow[s].x=100*j;
                     tab_rogow[s].y=100*(i+1);
                     tab_rogow[s].kat=270;
                     tab_rogow[s].kat_obrotu=180;
                     s++;
+		    }
                 }
                 if(tab_wejsciowa[i-1][j+1]==-9 && tab_wejsciowa[i][j+1]==0 && tab_wejsciowa[i-1][j]==0){
+		    c=1;
+                    for(k=1; k<=7; k++)
+                        if(tab_wejsciowa[i-k][j]==-9 || tab_wejsciowa[i][j+k]==-9)
+                                c=0;
+                    if(c==1){	
                     tab_rogow[s].x=100*(j+1);
                     tab_rogow[s].y=100*i;
                     tab_rogow[s].kat=270;
                     tab_rogow[s].kat_obrotu=0;
                     s++;
+		    }
                 }
                 if(tab_wejsciowa[i-1][j-1]==-9 && tab_wejsciowa[i][j-1]==0 && tab_wejsciowa[i-1][j]==0){
+		    c=1;
+                    for(k=1; k<=7; k++)
+                        if(tab_wejsciowa[i-k][j]==-9 || tab_wejsciowa[i][j-k]==-9)
+                                c=0;
+                    if(c==1){
                     tab_rogow[s].x=100*j;
                     tab_rogow[s].y=100*i;
                     tab_rogow[s].kat=270;
                     tab_rogow[s].kat_obrotu=90;
                     s++;
+		    }
                 }
             }
         }
