57915b0aab0446beb475c8d5a6246d269609c149
kowal2000 <59841365+kowal2000@users.noreply.github.com>
2020-04-11 11:30:06 +0200

    Dodanie zapisu położenia zraszczy do pliku

--- okrag.h
+++ okrag.h
@@ -6,4 +6,13 @@
 
 void stworz_kolo(int srodek_x, int srodek_y, int tab[ROZMIAR_Y][ROZMIAR_X], int kat, int kat_obrotu);
 
+typedef struct {
+        int x;
+        int y;
+        int kat;
+        int kat_obrotu;
+} okrag_t;
+okrag_t tab_okregow[1500];
+int indx;
+
 #endif OKRAG_H_INCLUDED
